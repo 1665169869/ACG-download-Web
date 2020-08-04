20841b31b0d3cd0676da2eed33a8de4e6aa10304
1665169869 <1665169869@qq.com>
2020-08-04 16:04:58 +0800

    更新了下链接

---
+++ docs/static/js/url.js
@@ -0,0 +1,17 @@
+var weChatWait=4;
+
+function qqqun(){
+  window.open("https://jq.qq.com/?_wv=1027&k=3d7wk5gW");
+}
+
+function qqemail(){
+  window.open("mailto:1665169869@qq.com")
+}
+
+function iwara(){
+    window.open("https://iwara-app.sanqianssr.com/")
+}
+
+function picacg(){
+    window.open("https://download.wikawika.xyz/apps/2.2.1.3.3.4_collections.apk")
+}
